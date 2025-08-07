7c9e263dd735db9e8ef16e42f2bea2ac1b1552a0
xingying <xingying_star@icould.com>
2025-08-07 21:20:29 +0800

    更新版本号为新版本，此版本编译版暂不发布！

--- Form1.cs
+++ Form1.cs
@@ -895,7 +895,7 @@ namespace sca
         {
             //消息提示框
             MessageBox.Show("学生电脑机房管理工具-SCA\n" +
-                "版本:1.0.7\n" +
+                "版本:1.0.8（Alpha）\n" +
                 "如果你觉得这个工具对你有帮助，欢迎在GitHub上给我一个Star⭐，这将极大地鼓励我继续开发更多有用的工具！\n\nGitHub仓库地址:https://github.com/fengliteam/SCA",
                 "感谢支持！！！", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
