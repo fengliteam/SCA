a1b80e9bb0349e59d0a15460bb33715721351048
xingying <xingying_star@icould.com>
2025-08-12 13:44:46 +0800

    UI换新

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,137 +40,188 @@
             button7 = new Button();
             button8 = new Button();
             checkBox1 = new CheckBox();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // 尝试破解
             // 
+            尝试破解.BackColor = Color.FromArgb(50, 51, 76);
+            尝试破解.FlatAppearance.BorderSize = 0;
+            尝试破解.FlatStyle = FlatStyle.Flat;
             尝试破解.Font = new Font("Microsoft YaHei UI", 42F, FontStyle.Regular, GraphicsUnit.Point, 134);
-            尝试破解.Location = new Point(96, 127);
+            尝试破解.ForeColor = Color.Gainsboro;
+            尝试破解.Location = new Point(183, 139);
             尝试破解.Name = "尝试破解";
             尝试破解.Size = new Size(334, 137);
             尝试破解.TabIndex = 1;
             尝试破解.Text = "点击破解";
-            尝试破解.UseVisualStyleBackColor = true;
+            尝试破解.UseVisualStyleBackColor = false;
             尝试破解.Click += 尝试破解_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(12, 329);
+            button1.BackColor = Color.FromArgb(50, 51, 76);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Gainsboro;
+            button1.Location = new Point(0, 44);
             button1.Name = "button1";
-            button1.Size = new Size(100, 23);
+            button1.Size = new Size(136, 38);
             button1.TabIndex = 2;
             button1.Text = "重启资源管理器";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(170, 359);
+            label1.ForeColor = Color.Gainsboro;
+            label1.Location = new Point(5, 342);
             label1.Name = "label1";
-            label1.Size = new Size(188, 17);
+            label1.Size = new Size(128, 17);
             label1.TabIndex = 3;
-            label1.Text = "以下为测试内容，稳定性无法保证";
+            label1.Text = "测试内容稳定性不保证";
             label1.Click += label1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(129, 329);
+            button2.BackColor = Color.FromArgb(50, 51, 76);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.Gainsboro;
+            button2.Location = new Point(0, 226);
             button2.Name = "button2";
-            button2.Size = new Size(99, 23);
+            button2.Size = new Size(136, 44);
             button2.TabIndex = 4;
             button2.Text = "恢复系统组件";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(238, 329);
+            button3.BackColor = Color.FromArgb(50, 51, 76);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.Gainsboro;
+            button3.Location = new Point(0, 132);
             button3.Name = "button3";
-            button3.Size = new Size(127, 23);
+            button3.Size = new Size(136, 38);
             button3.TabIndex = 5;
             button3.Text = "恢复 USB 设备访问";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(371, 329);
+            button4.BackColor = Color.FromArgb(50, 51, 76);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.Gainsboro;
+            button4.Location = new Point(0, 176);
             button4.Name = "button4";
-            button4.Size = new Size(221, 23);
+            button4.Size = new Size(136, 44);
             button4.TabIndex = 6;
             button4.Text = "重置Google和Edge浏览器管理策略";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(12, 300);
+            button5.BackColor = Color.FromArgb(50, 51, 76);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.Gainsboro;
+            button5.Location = new Point(0, 3);
             button5.Name = "button5";
-            button5.Size = new Size(261, 23);
+            button5.Size = new Size(136, 35);
             button5.TabIndex = 7;
             button5.Text = "禁止程序运行";
-            button5.UseVisualStyleBackColor = true;
+            button5.UseVisualStyleBackColor = false;
             button5.Click += button5_Click;
             // 
             // button6
             // 
-            button6.Location = new Point(294, 300);
+            button6.BackColor = Color.FromArgb(50, 51, 76);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.ForeColor = Color.Gainsboro;
+            button6.Location = new Point(0, 88);
             button6.Name = "button6";
-            button6.Size = new Size(298, 23);
+            button6.Size = new Size(136, 38);
             button6.TabIndex = 8;
             button6.Text = "重新允许运行";
-            button6.UseVisualStyleBackColor = true;
+            button6.UseVisualStyleBackColor = false;
             button6.Click += button6_Click;
             // 
             // button7
             // 
+            button7.BackColor = Color.FromArgb(50, 51, 76);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.ForeColor = Color.Gainsboro;
             button7.Location = new Point(502, 12);
             button7.Name = "button7";
             button7.Size = new Size(75, 23);
             button7.TabIndex = 9;
             button7.Text = "关于";
-            button7.UseVisualStyleBackColor = true;
+            button7.UseVisualStyleBackColor = false;
             button7.Click += button7_Click;
             // 
             // button8
             // 
-            button8.Location = new Point(266, 379);
+            button8.BackColor = Color.FromArgb(50, 51, 76);
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.ForeColor = Color.Gainsboro;
+            button8.Location = new Point(13, 362);
             button8.Name = "button8";
             button8.Size = new Size(112, 31);
             button8.TabIndex = 10;
             button8.Text = "检查更新";
-            button8.UseVisualStyleBackColor = true;
+            button8.UseVisualStyleBackColor = false;
             button8.Click += button8_Click;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Font = new Font("Microsoft YaHei UI", 15F);
-            checkBox1.Location = new Point(96, 379);
+            checkBox1.Font = new Font("Microsoft YaHei UI", 10F);
+            checkBox1.ForeColor = Color.Gainsboro;
+            checkBox1.Location = new Point(13, 389);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(151, 31);
+            checkBox1.Size = new Size(112, 24);
             checkBox1.TabIndex = 11;
             checkBox1.Text = "保持程序置顶";
             checkBox1.UseVisualStyleBackColor = true;
             checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(36, 35, 60);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.ForeColor = Color.Gainsboro;
+            panel1.Location = new Point(-1, -3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(136, 413);
+            panel1.TabIndex = 12;
             // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoValidate = AutoValidate.EnablePreventFocusChange;
+            BackColor = Color.FromArgb(36, 35, 55);
             ClientSize = new Size(589, 412);
-            Controls.Add(checkBox1);
-            Controls.Add(button8);
+            Controls.Add(panel1);
             Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(label1);
-            Controls.Add(button1);
             Controls.Add(尝试破解);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.FixedSingle;
@@ -179,8 +230,9 @@
             ImeMode = ImeMode.Off;
             MaximizeBox = false;
             Name = "Main";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -196,5 +248,6 @@
         private Button button7;
         private Button button8;
         private CheckBox checkBox1;
+        private Panel panel1;
     }
 }
